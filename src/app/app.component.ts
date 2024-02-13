41905c878266f8790d95905255fc7fb15f779ada
KreeZeG123 <manfaloti.yamis@gmail.com>
2024-02-13 14:16:26 +0100

    test composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet, MainComponent, NavBarComponent],
 })
 export class AppComponent {
   title = 'AnimeExplorer';
