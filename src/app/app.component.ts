feeb92b39fb6f9236cc3c4e0302b6f4127adc0d3
Raccouard Nathan <s2101613@ic2s124-10.univ-lemans.fr>
2024-02-13 14:07:37 +0100

    Quel code ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from '../components/nav-bar/nav-bar.component';
+import { SearchComponent } from '../components/search/search.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBarComponent,SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
