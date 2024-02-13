4208a3abde4748c6bbcd1e9eb3bab4447f7800c1
Theard Cody <s2101400@ic2s124-11.univ-lemans.fr>
2024-02-13 16:32:26 +0100

    Avance page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MainComponent } from './main/main.component';
+import { SearchComponent } from './search/search.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MainComponent,SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
