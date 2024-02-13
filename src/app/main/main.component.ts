7c16c4694bb3592ca4d95daacb99adfa72d4cfb8
KreeZeG123 <manfaloti.yamis@gmail.com>
2024-02-13 16:32:17 +0100

    Interaction NavBar & Board

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
+  imports: [CommonModule],
 })
 export class MainComponent {
+  boardId: string = 'boardJeu';
 
+  constructor(private navigationService: NavigationService) {
+    this.navigationService.navLinkClicked$.subscribe((boardId: string) => {
+      this.boardId = boardId;
+    });
+  }
 }
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { NavigationService } from '../navigation.service';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
   imports: [],
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss'
+  styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  activeNavLink: string = 'boardJeu';
 
+  constructor(private navigationService: NavigationService) {}
+
+  onNavLinkClick(boardId: string) {
+    this.activeNavLink = boardId;
+    this.navigationService.notifyNavLinkClicked(boardId);
+  }
 }
