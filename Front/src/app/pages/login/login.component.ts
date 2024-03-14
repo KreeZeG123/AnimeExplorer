167462914559822f32bb86fa7d4d604af5030c04
Yamis MANFALOTI <123065013+KreeZeG123@users.noreply.github.com>
2024-03-14 21:00:26 +0100

    Animation & Fonction de Login et Signup

--- Front/src/app/pages/login/login.component.ts
+++ Front/src/app/pages/login/login.component.ts
@@ -1,12 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NavBarComponent, RouterLink],
+  imports: [NavBarComponent, RouterLink, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  email: string = '';
+  emailValid: boolean | null = null;
+
+  password: string = '';
+  passwordValid: boolean | null = null;
+
+  emailIsValid(): void {
+    // TODO Requete a BDD -> recherche si email exist
+    // cherche username, recupère usernam + mdp crypté
+    if (this.email === '') {
+      this.emailValid = null;
+    } else if (!/\S+@\S+\.\S+/.test(this.email)) {
+      this.emailValid = false;
+    } else if (this.email.length >= 5) {
+      // Temporaire
+      this.emailValid = true;
+    } else {
+      this.emailValid = false;
+    }
+  }
+
+  passwordIsValid(): void {
+    // TODO Requete a BDD -> verifie si le password est correct
+    // crypte le mdp donnée, compare avec le mdp crypté correspondant a l'email
+    if (this.password === '') {
+      this.passwordValid = null;
+    } else if (this.password.length >= 8) {
+      // Temporaire
+      this.passwordValid = true;
+    } else {
+      this.passwordValid = false;
+    }
+  }
+
+  validateLogin(): void {
+    this.emailIsValid();
+    this.passwordIsValid();
+
+    if (this.emailValid && this.passwordValid) {
+      alert('Connexion avec succès');
+    } else {
+      alert('Veillez a bien remplir les champs correctement');
+    }
+  }
+}
--- Front/src/app/pages/signup/signup.component.ts
+++ Front/src/app/pages/signup/signup.component.ts
@@ -1,12 +1,78 @@
+import { CommonModule } from '@angular/common'; // Importez CommonModule pour ngClass
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importez FormsModule
 import { RouterLink } from '@angular/router';
 import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [NavBarComponent, RouterLink],
+  imports: [NavBarComponent, RouterLink, FormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  username: string = '';
+  usernameValid: boolean | null = null;
+
+  email: string = '';
+  emailValid: boolean | null = null;
+
+  passwordMain: string = '';
+  passwordGreaterThanEight: boolean = false;
+  passwordWithNumber: boolean = false;
+  passwordWithMaj: boolean = false;
+  passwordValid: boolean | null = null;
+
+  passwordSecondary: string = '';
+  passwordMatched: boolean | null = null;
+
+  checkUsernameValid(): void {
+    this.usernameValid = this.username.length >= 3;
+  }
+
+  checkEmailValid(): void {
+    this.emailValid = /\S+@\S+\.\S+/.test(this.email);
+  }
+
+  checkPasswordValid(): void {
+    this.passwordGreaterThanEight = this.passwordMain.length >= 8;
+    this.passwordWithNumber = /\d/.test(this.passwordMain);
+    this.passwordWithMaj = /[A-Z]/.test(this.passwordMain);
+
+    this.passwordValid =
+      this.passwordMain === ''
+        ? null
+        : this.passwordGreaterThanEight &&
+          this.passwordWithNumber &&
+          this.passwordWithMaj;
+  }
+
+  checkPasswordMatch(): void {
+    if (this.passwordSecondary === '') {
+      this.passwordMatched = null;
+    } else if (
+      this.passwordMain === this.passwordSecondary &&
+      this.passwordValid === true
+    ) {
+      this.passwordMatched = true;
+    } else {
+      this.passwordMatched = false;
+    }
+  }
+
+  validateSignup(): void {
+    if (
+      this.usernameValid &&
+      this.emailValid &&
+      this.passwordValid &&
+      this.passwordMatched
+    ) {
+      // Requete a BDD -> inscription nouveau utilisateur
+      // TODO
+      alert('Inscription validée');
+    } else {
+      alert('Veillez a completer le formulaire correctement');
+    }
+  }
+}
