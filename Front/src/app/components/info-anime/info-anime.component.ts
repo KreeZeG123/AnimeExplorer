0832e9bc6a3dbf10c7f6bed9aa0353beb5822775
Nathan Raccouard <tan2302@orange.fr>
2024-03-24 17:10:29 +0100

    l

--- Front/src/app/components/info-anime/info-anime.component.ts
+++ Front/src/app/components/info-anime/info-anime.component.ts
@@ -1,44 +1,42 @@
-import { Component, Input, Output,EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-info-anime',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './info-anime.component.html',
-  styleUrl: './info-anime.component.scss'
+  styleUrl: './info-anime.component.scss',
 })
 export class InfoAnimeComponent {
   @Input() info: any;
 
-  
-  nom_english: String="";
-  nom_jap: String="";
-  episodes: number=0;
-  desc: String="";
-  status: String="";
-  image: String="";
-  banner: String="";
-  studios: string[]=[]
-  score: number=0;
-  genres: string[]=[]
-  annee: number=0;
-  tags: string[]=[]
-  
+  nom_english: String = '';
+  nom_jap: String = '';
+  episodes: number = 0;
+  desc: String = '';
+  status: String = '';
+  image: String = '';
+  banner: String = '';
+  studios: string[] = [];
+  score: number = 0;
+  genres: string[] = [];
+  annee: number = 0;
+  tags: string[] = [];
+
   ngOnInit(): void {
     this.nom_english = this.info[0];
     this.nom_jap = this.info[1];
-    this.episodes= this.info[2];
-    this.desc=this.info[3].replaceAll("<br>",'\n').replaceAll("</i>","");
-    this.status=this.info[4];
-    this.image=this.info[5];
-    this.banner=this.info[6];
-    this.score=this.info[7];
-    this.studios=this.info[8];
-    this.genres=this.info[9];
-    this.annee=this.info[10];
-    this.tags=this.info[11];
-    console.log(this.studios);
+    this.episodes = this.info[2];
+    this.desc = this.info[3].replaceAll('<br>', '\n').replaceAll('</i>', '');
+    this.status = this.info[4];
+    this.image = this.info[5];
+    this.banner = this.info[6];
+    this.score = this.info[7];
+    this.studios = this.info[8];
+    this.genres = this.info[9];
+    this.annee = this.info[10];
+    this.tags = this.info[11];
   }
 
   @Output() buttonClicked = new EventEmitter<void>();
@@ -46,6 +44,4 @@ export class InfoAnimeComponent {
   onClick(): void {
     this.buttonClicked.emit();
   }
-
- 
 }
--- Front/src/app/components/nav-bar/nav-bar.component.ts
+++ Front/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { NavigationService } from '../../services/navigation.service';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
+  constructor(private authService: AuthService) {}
 
+  userIsConnected(): boolean {
+    return this.authService.userIsConnected();
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
